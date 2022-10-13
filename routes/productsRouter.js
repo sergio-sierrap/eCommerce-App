fef82f03a2522d0f1821f666051b115f863f43d2
sergio-sierrapgit config --global user.name sergio-sierrapgit config --global user.name sergio-sierrap <sergio_sierrap@outlook.com>
2022-10-13 18:18:37 -0500

    Add Generate and FindOne functions to productsService

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,18 +1,10 @@
 const express = require ('express');
-const faker = require('faker');
 const router = express.Router();
+const productsService = require('./../services/productsService');
+const service = new productsService();
 
 router.get('/', (req, res) => {
-    const products = [];
-    const {size} = req.query;
-    const limit = size || 10;
-    for (let index = 0; index <limit; index++){
-        products.push({
-            name: faker.commerce.productName(),
-            price: parseInt(faker.commerce.price(),10),
-            image: faker.image.imageUrl(),
-        });
-    }
+    const products = service.find();
     res.json(products);
 });
 
@@ -22,16 +14,13 @@ router.get('/filter', (req, res)=>{
 
 router.get('/:id', (req, res)=>{
     const { id } = req.params;
-    res.json({
-        id,
-        name: 'Product 2',
-        price: 2000
-    });
-})
+    const product = service.findOne(id);
+    res.json(product);
+});
 
 router.post('/', (req, res)=>{
     const body = req.body;
-    res.json({
+    res.status(201).json({
         message: 'Product created',
         data: body
     });
