105379006125c30edcca302699f5be51f92d0691
sergio-sierrapgit config --global user.name sergio-sierrapgit config --global user.name sergio-sierrap <sergio_sierrap@outlook.com>
2022-10-15 00:49:35 -0500

    último commit antes de Deployment

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/errorHandler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'https:myapp.co'];
+const options = {
+        origin: (origin, callback) => {
+        if (whitelist.includes(origin) || !origin ){
+        callback(null, true);
+        } else {
+            callback(new Error('no permitido'));
+        }
+    }
+}
+
+app.use(cors(options));
 
 app.get('/', (req, res) =>{
     res.send('Hola mi server en Express');
